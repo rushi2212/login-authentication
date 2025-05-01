8653030aa29423b280d101e89204a1e4c61eccbe
rushi2212 <rushikesh220703@gmail.com>
2025-05-01 14:09:48 +0530

    first commit

--- app.js
+++ app.js
@@ -1,85 +1,81 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const encrpypt = require('mongoose-encryption');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const encrpypt = require("mongoose-encryption");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/secrets");
+mongoose.connect(process.env.MONGO_URI);
 
 const trySchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
 
-const secret = "thisisasecret;"
+const secret = "thisisasecret;";
 trySchema.plugin(encrpypt, { secret: secret, encryptedFields: ["password"] });
 
 const item = mongoose.model("User", trySchema);
 
-app.get('/', function (req, res) {
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/register', function (req, res) {
-    res.render("register");
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/login", async function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/login", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    try {
-        const foundUser = await item.findOne({ email: username });
+  try {
+    const foundUser = await item.findOne({ email: username });
 
-        if (foundUser) {
-            if (foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                res.send("Incorrect password.");
-            }
-        } else {
-            res.send("User not found.");
-        }
-
-    } catch (err) {
-        console.log(err);
-        res.send("Error occurred while logging in.");
-    }
-});
-
-
-app.get('/login', function (req, res) {
-    res.render("login");
-});
-
-app.get('/logout', function (req, res) {
-            res.redirect('/');
-});
-
-
-app.post('/register', async function (req, res) {
-    try {
-        const newUser = new item({
-            email: req.body.username,
-            password: req.body.password
-        });
-
-        await newUser.save(); 
+    if (foundUser) {
+      if (foundUser.password === password) {
         res.render("secrets");
-
-    } catch (err) {
-        console.log(err);
-        res.send("Error occurred while registering.");
+      } else {
+        res.send("Incorrect password.");
+      }
+    } else {
+      res.send("User not found.");
     }
+  } catch (err) {
+    console.log(err);
+    res.send("Error occurred while logging in.");
+  }
 });
 
-app.listen(5000, function () {
-    console.log("Server Started");
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/logout", function (req, res) {
+  res.redirect("/");
+});
+
+app.post("/register", async function (req, res) {
+  try {
+    const newUser = new item({
+      email: req.body.username,
+      password: req.body.password,
+    });
+
+    await newUser.save();
+    res.render("secrets");
+  } catch (err) {
+    console.log(err);
+    res.send("Error occurred while registering.");
+  }
+});
+
+app.listen(port, function () {
+  console.log("Server Started");
 });
